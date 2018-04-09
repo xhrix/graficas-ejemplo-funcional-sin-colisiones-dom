12517a193cfbcc8b5b778835ecb6b02e53fa6a16
xhrix <xxxhrixxx@gmail.com>
2018-04-09 13:59:54 -0500

    Another chart demo.

--- ClientApp/components/Charts/ChartB.tsx
+++ ClientApp/components/Charts/ChartB.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as uuid from 'uuid/v4';
 
 interface ChartBProps {
     data: {
@@ -7,13 +8,86 @@ interface ChartBProps {
 }
 
 export class ChartB extends React.Component<ChartBProps, {}> {
+
+    private readonly chartUid: string;
+
+    constructor(props: ChartBProps) {
+        super(props);
+        this.chartUid = uuid();
+    };
+
+    componentDidMount() {
+        const that = this;
+        const Highcharts = (window as any).Highcharts;
+
+        Highcharts.chart(that.chartUid, {
+            chart: {
+                polar: true
+            },
+
+            title: {
+                text: 'Highcharts Polar Chart'
+            },
+
+            pane: {
+                startAngle: 0,
+                endAngle: 360
+            },
+
+            xAxis: {
+                tickInterval: 45,
+                min: 0,
+                max: 360,
+                labels: {
+                    formatter: function () {
+                        return (this as any).value + '°';
+                    }
+                }
+            },
+
+            yAxis: {
+                min: 0
+            },
+
+            plotOptions: {
+                series: {
+                    pointStart: 0,
+                    pointInterval: 45
+                },
+                column: {
+                    pointPadding: 0,
+                    groupPadding: 0
+                }
+            },
+
+            series: [{
+                type: 'column',
+                name: 'Column',
+                data: [8, 7, 6, 5, 4, 3, 2, 1],
+                pointPlacement: 'between'
+            }, {
+                type: 'line',
+                name: 'Line',
+                data: [1, 2, 3, 4, 5, 6, 7, 8]
+            }, {
+                type: 'area',
+                name: 'Area',
+                data: [1, 8, 2, 7, 3, 6, 4, 5]
+            }]
+        });
+    }
+
     public render() {
         return <div className="col-sm">
             <div className="jumbotron">
                 <h1 className="display-4">Graph {this.props.data.title}!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">Another graph.</p>
+                <hr className="my-4"/>
+                <div id={this.chartUid}
+                     style={{minWidth: '310px', maxWidth: '400px', height: '400px', margin: '0 auto'}}/>
+                <hr className="my-4"/>
+                <p>It uses utility classes for typography and spacing to space content out within the larger
+                    container.</p>
             </div>
         </div>;
     }
