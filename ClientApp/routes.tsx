16d498a26c20a3c38584a5b64b69b7c7a59acdad
xhrix <xxxhrixxx@gmail.com>
2018-04-09 14:15:00 -0500

    Redirect to /graphs

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Charts } from './components/Charts';
+import {Route} from 'react-router-dom';
+import {Layout} from './components/Layout';
+import {Charts} from './components/Charts';
+import {Redirect} from "react-router";
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Charts } />
+    <Route exact path='/graphs' component={Charts}/>
+    <Route exact path='/' component={() => <Redirect to="/graphs"/>}/>
 </Layout>;
