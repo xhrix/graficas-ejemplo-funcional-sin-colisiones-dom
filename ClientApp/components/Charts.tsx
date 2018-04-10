dd35543b29c49549919657fcb72f1766c2503c19
xhrix <xxxhrixxx@gmail.com>
2018-04-10 13:33:13 -0500

    Example of sortables.

--- ClientApp/components/Charts.tsx
+++ ClientApp/components/Charts.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
-import {ChartA} from './Charts/ChartA';
-import {ChartB} from './Charts/ChartB';
-import {ChartC} from "./Charts/ChartC";
-import {ChartD} from "./Charts/ChartD";
-import {ChartE} from "./Charts/ChartE";
+import {arrayMove, SortableContainer, SortableElement} from 'react-sortable-hoc';
+
+const SortableItem = SortableElement<{ value: string }>(({value}) => <li>{value}</li>);
+
+const SortableList = SortableContainer<{ items: string[] }>(({items}) => {
+    return (
+        <ul>
+            {(items).map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value}/>
+            ))}
+        </ul>
+    );
+});
+
+class SortableComponent extends React.Component {
+    state = {
+        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    };
+
+    onSortEnd = ({oldIndex, newIndex}: any) => {
+        this.setState({
+            items: arrayMove(this.state.items, oldIndex, newIndex),
+        });
+    };
+
+    render() {
+        return <SortableList axis={'xy'} items={this.state.items} onSortEnd={this.onSortEnd}/>;
+    }
+}
+
 
 interface Props {
     id?: number;
@@ -11,29 +36,6 @@ interface Props {
 
 export class Charts extends React.Component<Props> {
     public render() {
-        const {id} = this.props;
-        const single = !!id;
-
-        return <div>
-            <div className="container">
-                <div className="row">
-                    <div className={`col-sm xx-card ${single ? id == 1 ? 'xx-shown' : 'xx-hidden' : ''}`}>
-                        <ChartA data={{title: 'Chart A'}}/>
-                    </div>
-                    <div className={`col-sm xx-card ${single ? id == 2 ? 'xx-shown' : 'xx-hidden' : ''}`}>
-                        <ChartB data={{title: 'Chart B'}}/>
-                    </div>
-                    <div className={`col-sm mt-4 xx-card ${single ? id == 3 ? 'xx-shown' : 'xx-hidden' : ''}`}>
-                        <ChartC data={{title: 'Chart C'}}/>
-                    </div>
-                    <div className={`col-sm mt-4 xx-card ${single ? id == 4 ? 'xx-shown' : 'xx-hidden' : ''}`}>
-                        <ChartD data={{title: 'Chart D'}}/>
-                    </div>
-                    <div className={`col-sm mt-4 xx-card ${single ? id == 5 ? 'xx-shown' : 'xx-hidden' : ''}`}>
-                        <ChartE data={{title: 'Chart E'}}/>
-                    </div>
-                </div>
-            </div>
-        </div>;
+        return <SortableComponent/>;
     }
 }
