f1034157163ba28c089d2cfcb20b9620005ccd18
xhrix <xxxhrixxx@gmail.com>
2018-04-12 19:25:28 -0500

    Toggle urls.

--- ClientApp/components/LayoutBuilder/AvailableItems/AvailableItems.tsx
+++ ClientApp/components/LayoutBuilder/AvailableItems/AvailableItems.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import ChartsService from "../../../services/ChartsService";
+import LayoutBuilderService from "../../../services/LayoutBuilderService";
+import {observer} from "mobx-react";
 
+@observer
 export default class AvailableItems extends React.Component {
 
     async componentDidMount() {
@@ -10,16 +13,25 @@ export default class AvailableItems extends React.Component {
     }
 
     state = {
-        items: [],
+        items: [] as string[],
     };
 
     render() {
+        const l = LayoutBuilderService.Instance;
         return (
             <div className="AvailableItems">
 
                 <ul>
                     {this.state.items.map((x, i) => (
-                        <li key={`available-item-${i}`}>{x}</li>
+                        <li key={`available-item-${i}`}>
+                            <label>
+                                {x}
+                                <input type='checkbox'
+                                       checked={l.isSelected(x)}
+                                       onChange={() => l.toggleSelection(x)}
+                                />
+                            </label>
+                        </li>
                     ))}
                 </ul>
             </div>
--- ClientApp/services/LayoutBuilderService.ts
+++ ClientApp/services/LayoutBuilderService.ts
@@ -1,5 +1,36 @@
+import {observable} from "mobx";
+
 export default class LayoutBuilderService {
-    public static getSelectedCharts(): string[] {
-        return [];
+
+    @observable
+    private selectedCharts: string[] = [];
+
+    public get SelectedCharts(): string[] {
+        return this.selectedCharts;
+    }
+
+    public set SelectedCharts(value: string[]) {
+        this.selectedCharts = value || [];
+    }
+
+    public isSelected(value: string) {
+        return !!(this.selectedCharts.find(x => value === x));
+    }
+
+    public toggleSelection(value: string) {
+        if (this.isSelected(value)) {
+            this.selectedCharts = this.selectedCharts.filter(x => x !== value);
+        } else {
+            this.selectedCharts.push(value);
+        }
+    }
+
+    private static instance: LayoutBuilderService;
+
+    public static get Instance() {
+        if (!LayoutBuilderService.instance) {
+            LayoutBuilderService.instance = new LayoutBuilderService();
+        }
+        return LayoutBuilderService.instance;
     }
 }
