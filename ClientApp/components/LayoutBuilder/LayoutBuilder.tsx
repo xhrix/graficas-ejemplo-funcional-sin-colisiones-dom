fe752d9a06fc2bec9b90bc630576451e6f155e89
xhrix <xxxhrixxx@gmail.com>
2018-04-13 12:21:31 -0500

    Refactor, move 'SortableComponent' to its file.

--- ClientApp/components/LayoutBuilder/LayoutBuilder.tsx
+++ ClientApp/components/LayoutBuilder/LayoutBuilder.tsx
@@ -1,42 +1,7 @@
 import * as React from 'react';
-import {arrayMove, SortableContainer, SortableElement} from 'react-sortable-hoc';
 import * as styles from './LayoutBuilder.scss';
 import AvailableItems from "./AvailableItems/AvailableItems";
-import LayoutBuilderService from "../../services/LayoutBuilderService";
-import {observer} from "mobx-react";
-import ChartMeta from "../../models/ChartMeta";
-
-const SortableItem = SortableElement<{ value: ChartMeta }>(({value}) => (
-    <li className={styles.gridItem}>
-        <span className={styles.handle}/>
-        <iframe src={value.url}/>
-    </li>
-));
-
-const SortableList = SortableContainer<{ items: ChartMeta[] }>(({items}) => {
-    return (
-        <ul className={styles.grid}>
-            {(items).map((value, index) => (
-                <SortableItem key={`item-${index}`} index={index} value={value}/>
-            ))}
-        </ul>
-    );
-});
-
-@observer
-class SortableComponent extends React.Component {
-
-    private readonly layoutBuilderService = LayoutBuilderService.Instance;
-
-    onSortEnd = ({oldIndex, newIndex}: any) => {
-        this.layoutBuilderService.selectedCharts = arrayMove(this.layoutBuilderService.selectedCharts, oldIndex, newIndex);
-    };
-
-    render() {
-        return <SortableList axis={'xy'} items={this.layoutBuilderService.selectedCharts} onSortEnd={this.onSortEnd}/>;
-    }
-}
-
+import SortableComponent from "./SortableComponent";
 
 interface Props {
     id?: number;
