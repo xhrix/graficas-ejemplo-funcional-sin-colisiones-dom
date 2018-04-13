66e3d19a4f27613ed444f25e8a10976863ee298e
xhrix <xxxhrixxx@gmail.com>
2018-04-13 12:52:33 -0500

    Integrate sorts rezisables.

--- ClientApp/components/LayoutBuilder/GraphsCanvas.scss.d.ts
+++ ClientApp/components/LayoutBuilder/GraphsCanvas.scss.d.ts
@@ -1,3 +1,5 @@
 // Auto-generated code. Don't modify.
 
 export const item: string;
+export const itemWrapper: string;
+export const prevImg: string;
--- ClientApp/components/LayoutBuilder/GraphsCanvas.tsx
+++ ClientApp/components/LayoutBuilder/GraphsCanvas.tsx
@@ -2,23 +2,27 @@ import * as React from 'react';
 import * as ReactGridLayout from 'react-grid-layout';
 import * as styles from './GraphsCanvas.scss';
 import {Layout} from "react-grid-layout";
+import {observer} from "mobx-react";
+import LayoutBuilderService from "../../services/LayoutBuilderService";
 
-const layout: Layout[] = [
-    {i: 'a', x: 0, y: 0, w: 1, h: 1},
-    {i: 'b', x: 1, y: 0, w: 1, h: 1},
-    {i: 'c', x: 2, y: 0, w: 1, h: 1},
-    {i: 'd', x: 3, y: 0, w: 1, h: 1},
-];
-
-
+@observer
 export default class GraphsCanvas extends React.Component {
+    private readonly layoutBuilderService = LayoutBuilderService.Instance;
+
     render() {
+        const charts = this.layoutBuilderService.selectedCharts;
+        const layout: Layout[] = charts.map((x, i) => ({
+            i: `resizable-${x.url}`, x: i, y: 0, w: 1, h: 1
+        }));
         return (
             <ReactGridLayout className={'layout'} layout={layout} cols={12} rowHeight={90} width={1200}>
-                <div className={styles.item} key={'a'}>a</div>
-                <div className={styles.item} key={'b'}>b</div>
-                <div className={styles.item} key={'c'}>c</div>
-                <div className={styles.item} key={'d'}>d</div>
+                {charts.map(x => (
+                    <div className={styles.item} key={`resizable-${x.url}`}>
+                        <div className={styles.itemWrapper}>
+                            <span className={styles.prevImg} style={{backgroundImage: `url('${x.thumbnailUrl}')`}}/>
+                        </div>
+                    </div>
+                ))}
             </ReactGridLayout>
         );
     }
