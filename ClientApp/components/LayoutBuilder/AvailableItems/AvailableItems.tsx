5b44e116b985e04be094a1bec6790a6019e4a6ed
xhrix <xxxhrixxx@gmail.com>
2018-04-12 21:46:30 -0500

    css chido

--- ClientApp/components/LayoutBuilder/AvailableItems/AvailableItems.tsx
+++ ClientApp/components/LayoutBuilder/AvailableItems/AvailableItems.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import ChartsService from "../../../services/ChartsService";
 import LayoutBuilderService from "../../../services/LayoutBuilderService";
 import {observer} from "mobx-react";
+import * as styles from './AvailableItems.scss';
 
 @observer
 export default class AvailableItems extends React.Component {
@@ -21,15 +22,15 @@ export default class AvailableItems extends React.Component {
         return (
             <div className="AvailableItems">
 
-                <ul>
+                <ul className={styles.previews}>
                     {this.state.items.map((x, i) => (
-                        <li key={`available-item-${i}`}>
+                        <li className={styles.preview} key={`available-item-${i}`}>
                             <label>
-                                {x}
                                 <input type='checkbox'
                                        checked={l.isSelected(x)}
                                        onChange={() => l.toggleSelection(x)}
                                 />
+                                <span className={styles.previewImg} style={{backgroundImage: `url('${x}')`}}/>
                             </label>
                         </li>
                     ))}
