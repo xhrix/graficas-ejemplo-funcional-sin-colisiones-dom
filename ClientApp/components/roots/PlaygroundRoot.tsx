7c313c423c712ff4f48eb3a2340e9396fd528a97
xhrix <xxxhrixxx@gmail.com>
2018-04-16 16:48:58 -0500

    Use a HOC to encapsulate the complecity of saving a responsive sortable layout.

--- ClientApp/components/roots/PlaygroundRoot.tsx
+++ ClientApp/components/roots/PlaygroundRoot.tsx
@@ -1,36 +1,23 @@
 import * as React from 'react';
 import {RouteComponentProps} from "react-router";
-import * as ReactGridLayout from 'react-grid-layout';
-import * as styles from './PlaygroundRoot.scss';
-import AddRemoveAndLocalStorage from "../playground/react-grid-layout/AddRemoveAndLocalStorage";
+import {SortableSaveableGridHOC} from "../playground/react-grid-layout/SortableSaveableGridHOC";
 
-// layout is an array of objects, see the demo for more complete usage
-const layout = [
-    {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-    {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-    {i: 'c', x: 4, y: 0, w: 1, h: 2}
-];
-
-
-class MyFirstGrid extends React.Component {
+class GridItem extends React.Component {
     render() {
         return (
-            <ReactGridLayout className={'layout'} layout={layout} cols={12} rowHeight={30} width={1200}>
-                <div className={styles.item} key={'a'}>a</div>
-                <div className={styles.item} key={'b'}>b</div>
-                <div className={styles.item} key={'c'}>c</div>
-            </ReactGridLayout>
+            <span className="text">ELEMENTO</span>
         );
     }
 }
 
+const MyGrid = SortableSaveableGridHOC(GridItem);
 
 interface MatchProps {
 }
 
 const PlaygroundRoot = ({match}: RouteComponentProps<MatchProps>) => (
     <div>
-        <AddRemoveAndLocalStorage/>
+        <MyGrid/>
     </div>
 );
 
