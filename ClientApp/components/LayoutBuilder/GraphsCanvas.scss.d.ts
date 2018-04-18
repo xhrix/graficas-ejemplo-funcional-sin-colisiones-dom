748a8722665ecffd62940dbb382468133ca35195
xhrix <xxxhrixxx@gmail.com>
2018-04-18 15:57:12 -0500

    Remove old sortable.

--- ClientApp/components/LayoutBuilder/GraphsCanvas.scss.d.ts
+++
@@ -1,5 +0,0 @@
-// Auto-generated code. Don't modify.
-
-export const item: string;
-export const itemWrapper: string;
-export const prevImg: string;
--- ClientApp/components/LayoutBuilder/LayoutBuilder.tsx
+++ ClientApp/components/LayoutBuilder/LayoutBuilder.tsx
@@ -1,20 +1,61 @@
 import * as React from 'react';
 import * as styles from './LayoutBuilder.scss';
-import AvailableItems from "./AvailableItems/AvailableItems";
-import SortableComponent from "./SortableComponent";
-import GraphsCanvas from "./GraphsCanvas";
+import ChartMeta from "../../models/ChartMeta";
+import ChartsService from "../../services/ChartsService";
+import * as availablesStyles from './AvailableItems/AvailableItems.scss';
 
 interface Props {
-    id?: number;
 }
 
-export default class LayoutBuilder extends React.Component<Props> {
+interface State {
+    availableCharts: ChartMeta[];
+}
+
+export default class LayoutBuilder extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            availableCharts: []
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({
+            availableCharts: await ChartsService.getAvailableCharts()
+        });
+    }
+
+    private isSelected = (chartMeta: ChartMeta) => {
+        return false;
+    };
+
+    private toggleSelection = (chartMeta: ChartMeta) => {
+
+    };
+
+
     public render() {
         return (
             <div className={styles.container}>
-                <AvailableItems/>
-                <SortableComponent/>
-                <GraphsCanvas/>
+                <div className="AvailableItems">
+                    <ul className={availablesStyles.previews}>
+                        {this.state.availableCharts.map((x, i) => (
+                            <li className={availablesStyles.preview} key={`available-item-${i}`}>
+                                <label>
+                                    <input type='checkbox'
+                                           checked={this.isSelected(x)}
+                                           onChange={() => this.toggleSelection(x)}
+                                    />
+                                    <span className={availablesStyles.previewImg}
+                                          style={{backgroundImage: `url('${x.thumbnailUrl}')`}}>
+                                    <span>{x.url}</span>
+                                </span>
+                                </label>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         );
     }
