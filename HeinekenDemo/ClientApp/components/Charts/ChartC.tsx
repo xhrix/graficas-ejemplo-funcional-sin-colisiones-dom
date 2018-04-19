b06ff5476ff9f0ceec3a35f29648f59c009a7bc9
xhrix <xxxhrixxx@gmail.com>
2018-04-19 12:57:54 -0500

    Move files to project-like folder.

---
+++ HeinekenDemo/ClientApp/components/Charts/ChartC.tsx
@@ -0,0 +1,148 @@
+import * as React from 'react';
+import * as uuid from 'uuid/v4';
+import {NavLink} from "react-router-dom";
+
+interface Props {
+    data: {
+        title: string;
+    }
+}
+
+export class ChartC extends React.Component<Props, {}> {
+
+    private readonly chartUid: string;
+
+    constructor(props: Props) {
+        super(props);
+        this.chartUid = uuid();
+    };
+
+    componentDidMount() {
+        const that = this;
+        const Highcharts = (window as any).Highcharts;
+
+        Highcharts.chart(that.chartUid, {
+
+                chart: {
+                    type: 'gauge',
+                    plotBackgroundColor: null,
+                    plotBackgroundImage: null,
+                    plotBorderWidth: 0,
+                    plotShadow: false
+                },
+
+                title: {
+                    text: 'Speedometer'
+                },
+
+                pane: {
+                    startAngle: -150,
+                    endAngle: 150,
+                    background: [{
+                        backgroundColor: {
+                            linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                            stops: [
+                                [0, '#FFF'],
+                                [1, '#333']
+                            ]
+                        },
+                        borderWidth: 0,
+                        outerRadius: '109%'
+                    }, {
+                        backgroundColor: {
+                            linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                            stops: [
+                                [0, '#333'],
+                                [1, '#FFF']
+                            ]
+                        },
+                        borderWidth: 1,
+                        outerRadius: '107%'
+                    }, {
+                        // default background
+                    }, {
+                        backgroundColor: '#DDD',
+                        borderWidth: 0,
+                        outerRadius: '105%',
+                        innerRadius: '103%'
+                    }]
+                },
+
+                // the value axis
+                yAxis: {
+                    min: 0,
+                    max: 200,
+
+                    minorTickInterval: 'auto',
+                    minorTickWidth: 1,
+                    minorTickLength: 10,
+                    minorTickPosition: 'inside',
+                    minorTickColor: '#666',
+
+                    tickPixelInterval: 30,
+                    tickWidth: 2,
+                    tickPosition: 'inside',
+                    tickLength: 10,
+                    tickColor: '#666',
+                    labels: {
+                        step: 2,
+                        rotation: 'auto'
+                    },
+                    title: {
+                        text: 'km/h'
+                    },
+                    plotBands: [{
+                        from: 0,
+                        to: 120,
+                        color: '#55BF3B' // green
+                    }, {
+                        from: 120,
+                        to: 160,
+                        color: '#DDDF0D' // yellow
+                    }, {
+                        from: 160,
+                        to: 200,
+                        color: '#DF5353' // red
+                    }]
+                },
+
+                series: [{
+                    name: 'Speed',
+                    data: [80],
+                    tooltip: {
+                        valueSuffix: ' km/h'
+                    }
+                }]
+
+            },
+            // Add some life
+            function (chart: any) {
+                if (!chart.renderer.forExport) {
+                    setInterval(function () {
+                        var point = chart.series[0].points[0],
+                            newVal,
+                            inc = Math.round((Math.random() - 0.5) * 20);
+
+                        newVal = point.y + inc;
+                        if (newVal < 0 || newVal > 200) {
+                            newVal = point.y - inc;
+                        }
+
+                        point.update(newVal);
+
+                    }, 3000);
+                }
+            }
+        );
+    }
+
+    public render() {
+        return <div className="jumbotron">
+            <NavLink to={'/charts/3'}>
+                <h1 className="display-4">Graph {this.props.data.title}!</h1>
+            </NavLink>
+            <p className="lead">Another one.</p>
+            <div id={this.chartUid} style={{minWidth: '310px', maxWidth: '400px', height: '300px', margin: '0 auto'}}/>
+        </div>;
+    }
+}
