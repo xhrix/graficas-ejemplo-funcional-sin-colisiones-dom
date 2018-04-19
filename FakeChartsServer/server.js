06ff17b7769bb28027751a1563bc16e884227e5c
xhrix <xxxhrixxx@gmail.com>
2018-04-19 15:45:56 -0500

    Enable opt-in throttle to fake server.

--- FakeChartsServer/server.js
+++ FakeChartsServer/server.js
@@ -1,9 +1,16 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var port = process.env.port || 8080;
-connect().use(serveStatic(__dirname + '/public')).listen(port, function(){
-    console.log('Server running on ', port);
-});
+var port = process.env.PORT || 8080;
+var throttle = process.env.THROTTLE || 0;
+connect()
+    .use(function(request, response, next){
+        setTimeout(function() {
+            next();
+        }, throttle);
+    })
+    .use(serveStatic(__dirname + '/public')).listen(port, function () {
+        console.log('Server running on ', port, 'with throttle', throttle);
+    });
 
 // To set port on windows (PowerShell) and run server just:
 // $env:PORT=8811; node .\server.js
