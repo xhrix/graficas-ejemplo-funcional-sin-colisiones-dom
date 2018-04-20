4aed907cd9758da6e5abcebd5bda4b36ee971bc4
xhrix <xxxhrixxx@gmail.com>
2018-04-20 16:10:43 -0500

    Test with more workspaces, fix margin.

--- WorkspaceBuilder/ClientApp/mocks/workspace-mocks.ts
+++ WorkspaceBuilder/ClientApp/mocks/workspace-mocks.ts
@@ -15,5 +15,50 @@ export const WorkspaceMocks: Workspace[] = [
         id: 3,
         name: 'Workspace Three',
         previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 4,
+        name: 'Workspace Four',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 5,
+        name: 'Workspace Five',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 6,
+        name: 'Workspace Siz',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 7,
+        name: 'Workspace Seven',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 8,
+        name: 'Workspace Eight',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 9,
+        name: 'Workspace Nine',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 10,
+        name: 'Workspace Ten',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 11,
+        name: 'Workspace Eleven',
+        previewUrl: '/images/demo-charts-preview.png',
+    },
+    {
+        id: 12,
+        name: 'Workspace Twelve',
+        previewUrl: '/images/demo-charts-preview.png',
     }
 ];
