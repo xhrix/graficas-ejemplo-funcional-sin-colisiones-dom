83248daa46a5c14c16ff8e3728a5b2a72dde87ef
xhrix <xxxhrixxx@gmail.com>
2018-04-23 17:28:16 -0500

    Add mocks.

--- WorkspaceBuilder/ClientApp/mocks/workspace-mocks.ts
+++ WorkspaceBuilder/ClientApp/mocks/workspace-mocks.ts
@@ -1,64 +1,77 @@
 import Workspace from "../models/workspace";
+import {ChartMetaMocks} from "./chart-meta-mocks";
 
 export const WorkspaceMocks: Workspace[] = [
     {
         id: 1,
         name: 'Workspace One',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 2,
         name: 'Workspace Two',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 3,
         name: 'Workspace Three',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 4,
         name: 'Workspace Four',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 5,
         name: 'Workspace Five',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 6,
         name: 'Workspace Siz',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 7,
         name: 'Workspace Seven',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 8,
         name: 'Workspace Eight',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 9,
         name: 'Workspace Nine',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 10,
         name: 'Workspace Ten',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 11,
         name: 'Workspace Eleven',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     },
     {
         id: 12,
         name: 'Workspace Twelve',
         previewUrl: '/images/demo-charts-preview.png',
+        charts: ChartMetaMocks,
     }
 ];
