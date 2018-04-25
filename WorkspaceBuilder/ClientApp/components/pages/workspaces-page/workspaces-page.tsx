e4f13b740b696c0ed59bbefeb9de2454e615d744
xhrix <xxxhrixxx@gmail.com>
2018-04-25 18:11:33 -0500

    Main action button.

--- WorkspaceBuilder/ClientApp/components/pages/workspaces-page/workspaces-page.tsx
+++ WorkspaceBuilder/ClientApp/components/pages/workspaces-page/workspaces-page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Header from "../../header/header";
 import * as styles from './workspaces-page.scss';
+import * as mainButtonStyle from '../../styles/buttons/main-action-button.scss';
 import {WorkspaceMocks} from "../../../mocks/workspace-mocks";
 import WorkspacePreview from "../../workspace-preview/workspace-preview";
 
@@ -15,6 +16,10 @@ export default class WorkspacesPage extends React.Component {
                     {workspaces.map(workspace =>
                         <WorkspacePreview key={`work-space-${workspace.id}`} workspace={workspace}/>)}
                 </div>
+
+                <button className={mainButtonStyle.container} type="button">
+                    <i className="material-icons">add</i>
+                </button>
             </div>
         );
     }
