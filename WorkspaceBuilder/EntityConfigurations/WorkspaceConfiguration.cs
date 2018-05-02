569ade193828a258463d9779e4c1f7ca4dc5bc05
xhrix <xxxhrixxx@gmail.com>
2018-05-02 10:34:50 -0500

    Add workspace configuration.

---
+++ WorkspaceBuilder/EntityConfigurations/WorkspaceConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkspaceBuilder.Models;
+
+namespace WorkspaceBuilder.EntityConfigurations
+{
+    public class WorkspaceConfiguration : IEntityTypeConfiguration<Workspace>
+    {
+        public void Configure(EntityTypeBuilder<Workspace> builder)
+        {
+            builder.Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.HasIndex(m => m.Name).IsUnique();
+        }
+    }
+}
