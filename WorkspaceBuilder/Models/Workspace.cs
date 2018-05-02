af527aa330eae92027eb73ff88087079e5adfdbd
xhrix <xxxhrixxx@gmail.com>
2018-05-02 10:34:18 -0500

    Add workspace model.

---
+++ WorkspaceBuilder/Models/Workspace.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkspaceBuilder.Models
+{
+    public class Workspace
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Data { get; set; }
+    }
+}
