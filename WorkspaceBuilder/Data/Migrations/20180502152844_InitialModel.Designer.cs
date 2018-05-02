0141aec75a1f9f686b5662ee8d9f83d1d7289b80
xhrix <xxxhrixxx@gmail.com>
2018-05-02 10:30:18 -0500

    Add initial model migration.

--- WorkspaceBuilder/Data/Migrations/20180502152844_InitialModel.Designer.cs
+++ WorkspaceBuilder/Data/Migrations/20180502152844_InitialModel.Designer.cs
@@ -11,9 +11,10 @@ using WorkspaceBuilder.Data;
 namespace WorkspaceBuilder.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180502152844_InitialModel")]
+    partial class InitialModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
